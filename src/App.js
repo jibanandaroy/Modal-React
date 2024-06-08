ad3e40668660c13d401ad0603e8161f715625581
jibanandaroy <jibonroy282@gmail.com>
2024-06-08 10:36:24 +0600

    modal

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Modal from './component/modal/Modal';
+
+function App() {
+  return (
+    <div className="App">
+      <Modal/>
+    </div>
+  );
+}
+
+export default App;
